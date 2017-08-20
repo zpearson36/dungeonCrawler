6775c748fd426b186b6ce650250406edd3169ca1
Me <zpearson@hawk.iit.edu>
2017-08-20 14:59:50 -0500

    Added player movement via keys

--- game.py
+++ game.py
@@ -19,6 +19,7 @@ class Game:
         while not self._stop:
             self.getEvent()
             self._display.fill()
+            self._player.move()
             self._display.draw(self._player.getSprite(), self._player.getPos())
             self._display.update()
             self._clock.tick(60)
@@ -30,7 +31,19 @@ class Game:
             if event.type == pygame.QUIT:
                 self._stop = True
             if event.type == pygame.KEYDOWN:
-                pass
+                if event.key == pygame.K_LEFT:
+                        self._player.setVel((-5, 0))
+                elif event.key == pygame.K_RIGHT:
+                        self._player.setVel((5, 0))
+                elif event.key == pygame.K_UP:
+                        self._player.setVel((0, -5))
+                elif event.key == pygame.K_DOWN:
+                        self._player.setVel((0, 5))
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    self._player.setVel((0, 0))
+                elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    self._player.setVel((0, 0))
 
     def close(self):
         pygame.quit()
--- player.py
+++ player.py
@@ -4,6 +4,7 @@ class Player:
     def __init__(self, sprite = Sprite(), pos = (300, 300)):
         self._sprite = sprite
         self._pos = pos
+        self._vel = (0, 0)
 
     def getSprite(self):
         return self._sprite
@@ -15,3 +16,14 @@ class Player:
         if pos == None:
             pos = self.getPos()
         self._pos = pos
+
+    def setVel(self, vect = (0,0)):
+        self._vel = vect
+
+    def getVel(self):
+        return self._vel
+
+    def move(self):
+        newX = self.getPos()[0]+self.getVel()[0]
+        newY = self.getPos()[1]+self.getVel()[1]
+        self.setPos((newX, newY))
