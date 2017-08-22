e8c9e72b1b4bb77ba2709ddbd96ee9e0416487a0
Me <zpearson@hawk.iit.edu>
2017-08-21 21:46:24 -0500

    Smaller Sprites

--- sprite.py
+++ sprite.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Sprite:
-    def __init__(self, img ="assets/sprites/player2.png", size=(53,68)):
+    def __init__(self, img ="assets/sprites/player.png", size=(53,68)):
         self._image = pygame.image.load(img)
         self._size = size
 
